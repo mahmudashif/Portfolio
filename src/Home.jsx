e4ce084a37160e346e55136ae921bf406a3366dd
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-23 23:53:38 +0600

    intro and portrait done

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,12 @@ import Navbar from './Navbar'
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
+    <div className='bg-primary'>
+        <div className='font-imbue text-center pt-[77px]'>
+            <p className='text-[20px] text-white pb-6'>My Intro</p>
+            <h3 className='text-white text-[45px] uppercase font-bold'>Self Portrait</h3>
+        </div>
+        
     </div>
   )
 }
