9c99c8b8cf7830924b6fbf69d5c0d061b889f19b
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-23 23:58:28 +0600

    pic add in the part of about

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import Navbar from './Navbar'
+import aboutPic from "../src/assets/about.png"
 
 const Home = () => {
   return (
     <div className='bg-primary'>
-        <div className='font-imbue text-center pt-[77px]'>
+        <div className='font-imbue text-center pt-[77px] container'>
             <p className='text-[20px] text-white pb-6'>My Intro</p>
             <h3 className='text-white text-[45px] uppercase font-bold'>Self Portrait</h3>
+        </div>
+        <div className='container'>
+            <img src={aboutPic} alt="" />
         </div>
         
     </div>
